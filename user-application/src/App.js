946f3042602a974f83240b515bf4ae7c94c1e6ad
Mikael Sahleström <mikael.sahlestrom@gmail.com>
2021-04-16 11:44:42 +0200

    init react files

---
+++ user-application/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Nora from './components/Nora'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="test">
+        <p> Micke är cool</p>
+        <Nora />
+        <Nora />
+        <Nora />
+        <Nora />
+        <Nora />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-application/src/components/Nora.js
@@ -0,0 +1,7 @@
+function Nora(){
+    return(
+        <div onClick={()=>console.log("hello Nora")}> Nora är också bra</div>
+    )
+}
+
+export default Nora
