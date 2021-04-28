62e566468a4116df23dffb7c83e5ca3e82438ef9
emilsjol <emilsjol@kth.se>
2021-04-28 12:32:40 +0200

    Lägga till förmåga att rita arrayer i Canvas.js

--- user-application/src/components/UI/Canvas.js
+++ user-application/src/components/UI/Canvas.js
@@ -3,8 +3,8 @@ import React, { useRef, useEffect } from "react";
 /*
 This component draws a canvas using html canvas tag.
 It's based on this article:  https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258#_=_
-
 */
+
 const Canvas = (props) => {
   const canvasRef = useRef(null);
 
@@ -24,6 +24,26 @@ const Canvas = (props) => {
     ctx.stroke();
   };
 
+  const drawArray = (ctx, arr) => {
+    let max_y = ctx.canvas.height;
+    let max_x = ctx.canvas.width;
+    let width = 10;
+    for (let i = 0; i < arr.length; i++) {
+      ctx.rect(i * width, max_y - (arr[i] * 5), width, arr[i] * 5);
+      ctx.fill();
+    }
+  };
+
+  const drawMatrix = (ctx, mtrx) => {
+    for (let i = 0; i < mtrx.length; i++) {
+      for (let j = 0; j < mtrx[i].length; j++) {
+        let value = mtrx[i][j];
+        ctx.rect(value * 10, 10, 10, value * 10);
+        ctx.fill();
+      }
+    }
+  }
+
   // Since we want the function to self trigger a render, we want to dissable the warning
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const animation = (ctx, frameCount) => {
@@ -39,20 +59,38 @@ const Canvas = (props) => {
     let frameCount = 0;
     let animationFrameId;
 
+    var matrix = [];
+    for (let i = 0; i < 100; i++) {
+      matrix[i] = [];
+      for (let j = 0; j < 100; j++) {
+        matrix[i][j] = j;
+      }
+    }
+    
     const render = () => {
       frameCount++;
       context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-      animation(context, frameCount);
-      drawTriangle(context);
-      drawRectangle(context);
-      animationFrameId = window.requestAnimationFrame(render);
+
+      var arr = [];
+      for (let i = 0; i < 100; i++) {
+        arr[i] = Math.floor(Math.random() * 100);
+      }
+
+      drawArray(context, arr);
+      //drawMatrix(context, matrix);
+
+      //animation(context, frameCount);
+      //drawTriangle(context);
+      //drawRectangle(context);
+      //animationFrameId = window.requestAnimationFrame(render);
     };
     render();
 
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [animation]);
+  }, []);
+  //}, [animation]);
 
   return <canvas ref={canvasRef} width="500" height="500" {...props} />;
 };
