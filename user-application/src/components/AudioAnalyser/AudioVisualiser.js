82fc5a68a33ef29c4feb7d68eb9824f4a918f144
nodelius <nodelius@kth.se>
2021-05-05 15:29:38 +0200

    git strugle

--- user-application/src/components/AudioAnalyser/AudioVisualiser.js
+++ user-application/src/components/AudioAnalyser/AudioVisualiser.js
@@ -7,46 +7,39 @@ class AudioVisualiser extends Component {
         this.canvas = React.createRef();
     }
 
-    
     draw() {
-
-
         const {frequencyData} = this.props;
         const canvas1 = this.canvas.current;
         const height = canvas1.height;
         const width = canvas1.width;
         const context = canvas1.getContext('2d');
-        let x = 0;
-        const df = this.props.df;
         const sliceWidth = (width * 2.0)/frequencyData.length;
+        const upperLimit = 2500;
 
-
-
-        context.lineWidth = 1;
-        context.strokeStyle = '#044599';
-        context.clearRect(0,0,width, height);
-        context.beginPath();
+        context.clearRect(0, 0, width, height);
 
         var n = frequencyData.length;
-        for (var i = 1; i<n/2; i++) {
 
-            if (frequencyData[i] > 0) {
-                context.moveTo(x, 255);
-                context.lineTo(x, 255-frequencyData[i]);            
+        console.log(frequencyData[0]);
+
+        const drawRect = (ctx, pos, width, height, colour) => {
+            ctx.fillStyle = colour;
+            ctx.beginPath();
+            ctx.rect(pos, ctx.canvas.height, width, -height);
+            ctx.fill();
+          };
+
+        for (var i = 1; i<n/2 + 1; i++) {
+            if (frequencyData[i]>0) {
+                if (frequencyData[i] >= upperLimit) {
+                    drawRect(context, i * sliceWidth, sliceWidth, height, 'red');
+                }
+                else {
+                    var barHeightPercentage = frequencyData[i] / upperLimit;
+                    drawRect(context, i * sliceWidth, sliceWidth, barHeightPercentage * height, 'blue');
+                }           
             }
-
-            x += sliceWidth;
-
         }
-
-        context.fillText("0 hz", 0, height);
-        context.fillText("4096 hz", 760, height)
-        context.moveTo(0, height);
-        context.lineTo(width, height);
-        context.stroke();
-
-
-
     }
 
     componentDidUpdate() {
@@ -54,7 +47,7 @@ class AudioVisualiser extends Component {
       }
 
     render() {
-        return <canvas width="800" height="300" ref={this.canvas}/>
+        return <canvas width="1024" height="500" ref={this.canvas}/>
     }
 }
 
