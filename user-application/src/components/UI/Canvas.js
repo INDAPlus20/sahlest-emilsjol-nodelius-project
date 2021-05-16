45aabafa70ab24565d9073547c44cb900e668ca8
Mikael Sahleström <mikael.sahlestrom@gmail.com>
2021-05-16 11:38:51 +0200

    move animation effect methods into the useEffect hook

--- user-application/src/components/UI/Canvas.js
+++ user-application/src/components/UI/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 /*
 This component draws a canvas using html canvas tag.
@@ -10,7 +10,7 @@ const Canvas = (props) => {
   const mtrxRef = useRef(0);
   let framesPerSecond = 5;
   // let runAnimation = false;
-  const [runAnimation, runAnimationSetstate] = useState(false)
+  const [runAnimation, runAnimationSetstate] = useState(false);
 
   function generateMatrixFromJson(data) {
     let mtrx = [];
@@ -23,40 +23,21 @@ const Canvas = (props) => {
     mtrxRef.current = mtrx;
   }
 
-  const drawRect = (ctx, pos, width, height) => {
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.rect(pos, ctx.canvas.height, width, -height);
-    ctx.fill();
-  };
 
-  const drawArray = (ctx, frameCount, mtrx) => {
-    let currentRow = frameCount % mtrx.length;
-    let width = ctx.canvas.width / mtrx[currentRow].length;
-    for(const [i, barHeight] of mtrx[currentRow].entries()){
-      drawRect(ctx, i*width, width, barHeight * 650)
-    }
-  }
-  
-  //ogillar att mtrx måste tas som argument men behövs för att längden på staplarna ska följa canvas-storleken
-  const animation = (ctx, frameCount, mtrx) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    drawArray(ctx, frameCount, mtrx);
-  };
-  
   //sker en gång, notera tomma klamrarna i slutet
   useEffect(() => {
-
-      fetch("http://localhost:8080/matrix")
-        .then(response => response.json())
-        .then(data => generateMatrixFromJson(data))
-        .finally(runAnimationSetstate(true))
-        .catch(error => alert(error));
-
+    fetch("http://localhost:8080/matrix")
+      .then((response) => response.json())
+      .then((data) => generateMatrixFromJson(data))
+      .finally(runAnimationSetstate(true))
+      .catch((error) => alert(error));
   }, []); // <--
 
   //för säkerhets skull
-  let mtrxTemp = [[1, 1], [1, 1]];
+  let mtrxTemp = [
+    [1, 1],
+    [1, 1],
+  ];
   mtrxRef.current = mtrxTemp;
 
   //körs varje gång animation, runAnimation eller framesPerSecond ändras
@@ -67,6 +48,28 @@ const Canvas = (props) => {
     const context = canvas.getContext("2d");
     let frameCount = 0;
     let animationFrameId;
+
+    //ogillar att mtrx måste tas som argument men behövs för att längden på staplarna ska följa canvas-storleken
+    const animation = (ctx, frameCount, mtrx) => {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      drawArray(ctx, frameCount, mtrx);
+    };
+
+
+  const drawRect = (ctx, pos, width, height) => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.rect(pos, ctx.canvas.height, width, -height);
+    ctx.fill();
+  };
+
+  const drawArray = (ctx, frameCount, mtrx) => {
+    let currentRow = frameCount % mtrx.length;
+    let width = ctx.canvas.width / mtrx[currentRow].length;
+    for (const [i, barHeight] of mtrx[currentRow].entries()) {
+      drawRect(ctx, i * width, width, barHeight * 650);
+    }
+  };
 
     const render = () => {
       if (runAnimation) {
@@ -82,7 +85,7 @@ const Canvas = (props) => {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [animation, runAnimation, framesPerSecond]); // <-- icke-tomma klamrar
+  }, [runAnimation, framesPerSecond]); // <-- icke-tomma klamrar
 
   return <canvas ref={canvasRef} width="1200" height="650" {...props} />;
 };
