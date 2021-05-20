0605f1c4b2945e83d6df72c00a221693781f18b3
emilsjol <emilsjol@kth.se>
2021-05-20 19:06:58 +0200

    Ändrade så att sidan refreshas när man byter flik

--- user-application/src/components/UI/Navbar.js
+++ user-application/src/components/UI/Navbar.js
@@ -1,25 +1,34 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, BrowserRouter as Router } from "react-router-dom";
 
 function Navbar() {
   return (
     <header className={styles.navbar}>
       <nav >
         <ul>
+          <Router forceRefresh>
+            <li>
+                <NavLink activeClassName={styles.active} to="/" exact>Home</NavLink>
+            </li>
+          </Router>
           <li>
-            <NavLink activeClassName={styles.active} to="/" exact>Home</NavLink>
+            <Router forceRefresh>
+              <NavLink activeClassName={styles.active} to="/AudioRecording">Audio Recording</NavLink>
+            </Router>
           </li>
           <li>
-            <NavLink activeClassName={styles.active} to="/AudioRecording">Audio Recording</NavLink>
+            <Router forceRefresh>
+              <NavLink activeClassName={styles.active} to="/FileAnalyzer">File Analyzer</NavLink>
+            </Router>
           </li>
           <li>
-            <NavLink activeClassName={styles.active} to="/FileAnalyzer">File Analyzer</NavLink>
+            <Router forceRefresh>
+              <NavLink activeClassName={styles.active} to="/Chatbot">Chatbot</NavLink>
+            </Router>
           </li>
-          <li>
-            <NavLink activeClassName={styles.active} to="/Chatbot">Chatbot</NavLink>
-          </li>
+          
         </ul>
       </nav>
     </header>
